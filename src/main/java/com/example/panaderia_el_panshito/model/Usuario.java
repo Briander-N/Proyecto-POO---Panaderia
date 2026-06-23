2ad88ca4c2fb63ee88c7ca75f12893f6311403a3
alejandro4418 <alejandro.aguirre@epn.edu.ec>
2026-06-22 19:48:13 -0500

    Modelo de clases - Alejandro Aguirre

--- src/main/java/com/example/panaderia_el_panshito/model/Usuario.java
+++ src/main/java/com/example/panaderia_el_panshito/model/Usuario.java
@@ -1,4 +1,40 @@
 package com.example.panaderia_el_panshito.model;
 
-public class Usuario {
+public class Usuario extends Persona {
+
+    private String password;
+    private String rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombre, String correo,
+                   String password, String rol) {
+
+        super(id, nombre, correo);
+
+        this.password = password;
+        this.rol = rol;
+    }
+
+    @Override
+    public String obtenerRol() {
+        return rol;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
