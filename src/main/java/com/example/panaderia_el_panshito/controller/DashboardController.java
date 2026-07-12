d1c46b17cda1bc253f490c48a90e2a301e7a74ab
Briander <pochitobriander@gmail.com>
2026-07-12 17:42:49 -0500

    Dashboard fxml y controller

--- src/main/java/com/example/panaderia_el_panshito/controller/DashboardController.java
+++ src/main/java/com/example/panaderia_el_panshito/controller/DashboardController.java
@@ -1,4 +1,55 @@
 package com.example.panaderia_el_panshito.controller;
 
+import com.example.panaderia_el_panshito.model.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class DashboardController {
+
+    @FXML private Label lblNombre;
+    @FXML private Label lblRol;
+
+    @FXML private VBox panelAdmin;
+    @FXML private VBox panelCajero;
+    @FXML private VBox panelReportes;
+
+    private Usuario usuario;
+
+    // Este método lo llama el LoginController al entrar
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        lblNombre.setText("Usuario: " + usuario.getNombre());
+        lblRol.setText("Rol: " + usuario.getRol());
+        adaptarMenuPorRol();
+    }
+
+    private void adaptarMenuPorRol() {
+        String rol = usuario.getRol();
+
+        ocultar(panelAdmin);
+        ocultar(panelCajero);
+        ocultar(panelReportes);
+
+        switch (rol) {
+            case "ADMIN" -> mostrar(panelAdmin);
+            case "CAJERO" -> mostrar(panelCajero);
+            case "REPORTES" -> mostrar(panelReportes);
+        }
+    }
+
+    private void mostrar(VBox panel) {
+        panel.setVisible(true);
+        panel.setManaged(true);
+    }
+
+    private void ocultar(VBox panel) {
+        panel.setVisible(false);
+        panel.setManaged(false);
+    }
+
+    @FXML
+    private void cerrarSesion() {
+        // TODO: siguiente entrega -> volver a cargar login.fxml y cerrar sesión
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.panaderia_el_panshito {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.panaderia_el_panshito to javafx.fxml;
