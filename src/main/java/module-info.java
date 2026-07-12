585de3ba2cbbbe342f5894d3766af64e92c1271d
Briander <pochitobriander@gmail.com>
2026-07-12 17:57:46 -0500

    Correcion para ejecución

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module com.example.panaderia_el_panshito {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens com.example.panaderia_el_panshito to javafx.fxml;
+    opens com.example.panaderia_el_panshito.controller to javafx.fxml;
+    opens com.example.panaderia_el_panshito.model to javafx.fxml;
+
     exports com.example.panaderia_el_panshito;
 }
